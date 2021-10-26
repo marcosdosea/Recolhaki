dd98b83d55825a8911967a3b6dfb05bfb7c11b13
JessicaPortilio <47065396+JessicaPortilio@users.noreply.github.com>
2021-10-26 20:59:13 -0300

    Revert "Branch_Jessica 62"

---
+++ Codigo/RecolhakiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecolhakiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
