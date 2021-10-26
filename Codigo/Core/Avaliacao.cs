541521e51eb0ea63da22748a2e89586d62b1d72d
JessicaPortilio <jessicaket91@gmail.com>
2021-10-26 20:58:00 -0300

    Foi feito a recriação da aplicação conforme o issues clone #62

---
+++ Codigo/Core/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Avaliacao
+    {
+        public int IdAvaliacao { get; set; }
+        public string Descricao { get; set; }
+        public int IdEmoje { get; set; }
+        public int PessoaIdPessoa { get; set; }
+        public int DoacaoMaterialReciclavelIdDoacaoMaterialReciclavel { get; set; }
+
+        public virtual Doacaomaterialreciclavel DoacaoMaterialReciclavelIdDoacaoMaterialReciclavelNavigation { get; set; }
+        public virtual Pessoa PessoaIdPessoaNavigation { get; set; }
+    }
+}
