3fe5fd8e8faeda4f15ceeb551f68c4e2d61694b8
Nathan Santos <nathanaelsantos15@gmail.com>
2021-10-26 19:10:43 -0300

    Implementacao da conexao do banco de dados close #60

--- Codigo/RecolhakiWeb/Controllers/HomeController.cs
+++ Codigo/RecolhakiWeb/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RecolhakiWeb.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RecolhakiWeb.Controllers
 {
