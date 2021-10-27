b3f0d841690198528f4f5c50745fa465f885a836
JessicaPortilio <jessicaket91@gmail.com>
2021-10-26 23:00:39 -0300

    Refazendo issues clone #65

---
+++ Codigo/Core/Doacaomaterialreciclavel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Doacaomaterialreciclavel
+    {
+        public Doacaomaterialreciclavel()
+        {
+            Avaliacao = new HashSet<Avaliacao>();
+        }
+
+        public int IdDoacaoMaterialReciclavel { get; set; }
+        public int Tipo { get; set; }
+        public string Nome { get; set; }
+        public float Peso { get; set; }
+        public DateTime DataManifestacaoInteresse { get; set; }
+        public int PessoaIdPessoa { get; set; }
+        public int NotificacaoIdNotificacao { get; set; }
+        public int ColetorIdColetor { get; set; }
+
+        public virtual Coletor ColetorIdColetorNavigation { get; set; }
+        public virtual Notificacao NotificacaoIdNotificacaoNavigation { get; set; }
+        public virtual Pessoa PessoaIdPessoaNavigation { get; set; }
+        public virtual ICollection<Avaliacao> Avaliacao { get; set; }
+    }
+}
