8aea7aafa07996e20251f698cff713049273bebb
Nathan Santos <nathanaelsantos15@gmail.com>
2021-11-05 10:13:47 -0300

    Criado classe de teste para a classe disponibilizarMaterial close #88

---
+++ Codigo/RecolhakiWebTests/Controllers/DisponibilizarMaterialControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecolhakiWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecolhakiWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class DisponibilizarMaterialControllerTests
+    {
+        [TestMethod()]
+        public void DisponibilizarMaterialControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Codigo/ServiceTests/DisponibilizarMaterialServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class DisponibilizarMaterialServiceTests
+    {
+        [TestMethod()]
+        public void DisponibilizarMaterialServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getMaterialTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            Assert.Fail();
+        }
+
+        public void ObterPorNomeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObterTodosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoverTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
