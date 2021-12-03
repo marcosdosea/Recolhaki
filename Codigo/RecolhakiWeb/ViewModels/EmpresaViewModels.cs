f7b49db606c803f9e2cff025d19f8f54486a2189
JessicaPortilio <jessicaket91@gmail.com>
2021-12-02 22:50:12 -0300

    Continuando o caso de uso Manter Empresa

---
+++ Codigo/RecolhakiWeb/ViewModels/EmpresaViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecolhakiWeb.ViewModels
+{
+    public class EmpresaViewModels
+    {
+        [Required]
+        [Key]
+        public int IdEmpresa { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(15, MinimumLength = 10, ErrorMessage = "CNPJ")]
+        public string Cnpj { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Nome da empresa deve ter entre 5 e 45 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Email")]
+        public string Rua { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Cep")]
+        public int Cep { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "Numero")]
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Complemento")]
+        public string Complemento { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Cidade")]
+        public string Cidade { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Bairro")]
+        public string Bairro { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 2, ErrorMessage = "Estado")]
+        public string Estado { get; set; }
+    }
+}
---
+++ Codigo/Service/EmpresaService.cs
@@ -0,0 +1,90 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class EmpresaService : IEmpresaService
+    {
+        private readonly recolhakiContext _context;
+
+        public EmpresaService(recolhakiContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Atualiza os dados da Empresa na base de dados
+        /// </summary>
+        /// <param name="empresa">dados da empresa</param>
+        public void Editar(Empresa empresa)
+        {
+            _context.Update(empresa);
+            _context.SaveChanges();
+        }
+        /// <summary>
+        /// Inserir nova Empresa
+        /// </summary>
+        /// <param name="empresa">dados da empresa</param>
+        /// <returns>retorna o IdEmpresa gerado</returns>
+        public int Inserir(Empresa empresa)
+        {
+            _context.Add(empresa);
+            _context.SaveChanges();
+            return empresa.IdEmpresa;
+        }
+        /// <summary>
+        /// Buscar empresa pelo id na base de dados
+        /// </summary>
+        /// <param name="IdEmpresa">id da empresa que será buscado</param>
+        /// <returns>Os dados da empresa ou nulo quando não encontrado</returns>
+        public Empresa Obter(int IdEmpresa)
+        {
+            return _context.Pessoa.Find(IdEmpresa);
+
+        }
+
+        public IEnumerable<Empresa> ObterPorNome(string nome)
+        {
+
+            var query = from empresa in _context.Empresa
+                        orderby empresa.Nome
+                       select empresa;
+            return query;
+        }
+
+        public IEnumerable<EmpresaDTO> ObterPorNomeOrdenadoDescendign(string nome)
+        {
+            var query = from empresa in _context.Empresa
+                         where empresa.Nome.StartsWith(nome)
+                        orderby empresa.Nome descending
+                      select new EmpresaDTO
+                       {
+                            Nome = empresa.Nome
+                        };
+              return query;
+            
+        }
+
+        public IEnumerable<Empresa> ObterTodos()
+        {
+            return _context.Pessoa;
+        }
+
+        /// <summary>
+        /// Remove a empresa da base de dados
+        /// </summary>
+        /// <param name="IdPessoa">a ser removido</param>
+
+        public void Remover(int IdEmpresa)
+        {
+            var empresa = _context.Empresa.Find(IdEmpresa);
+            _context.Remove(empresa);
+            _context.SaveChanges();
+        }
+    }
+}
