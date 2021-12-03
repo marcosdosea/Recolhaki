9ecd46d3631e66a2acda3ef2d6df32507168f9c6
Nathan Santos <nathanaelsantos15@gmail.com>
2021-12-03 07:57:35 -0300

    Adicionado class MaterialreciclavelProfile

---
+++ Codigo/RecolhakiWeb/Mappers/MaterialreciclavelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using RecolhakiWeb.ViewModels;
+
+namespace RecolhakiWeb.Mappers
+{
+    public class MaterialreciclavelProfile : Profile
+    {
+        public MaterialreciclavelProfile()
+        {
+            CreateMap<MaterialreciclavelViewModel, Materialreciclavel>().ReverseMap();
+        }
+    }
+}
