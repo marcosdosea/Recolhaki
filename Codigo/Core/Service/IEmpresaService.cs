39e55f8028f1e39713251e0274479bb13ef3c6e7
JessicaPortilio <jessicaket91@gmail.com>
2021-12-02 22:04:10 -0300

    Criando Empresa DTO e IEmpresaService

---
+++ Codigo/Core/Service/IEmpresaService.cs
@@ -0,0 +1,22 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IEmpresaService
+    {
+        void Editar(Empresa empresa);
+        int Inserir(Empresa empresa);
+        Empresa Obter(int IdEmpresa);
+        IEnumerable<Empresa> ObterPorNome(string nome);
+        IEnumerable<Empresa> ObterTodos();
+        void Remover(int IdEmpresa);
+        IEnumerable<EmpresaDTO> ObterPorNomeOrdenadoDescendign(string nome);
+
+        
+    }
+}
